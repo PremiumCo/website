96d44bd55b0c063786e90685c4970e6d1785c574
2xelevate <misteats@gmail.com>
2024-10-08 21:41:08 -0600

    new metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ const features = [
   {
     name: '',
     description:
-      'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
+      '',
     icon: CloudArrowUpIcon,
   },
   {
@@ -73,7 +73,7 @@ export default function Example() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <a href="/login" className="text-sm font-semibold leading-6 text-gray-900">
               Log in <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
