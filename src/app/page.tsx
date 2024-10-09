bb7f177594f18a98d55266c136ed7799ea88bc6c
2xelevate <misteats@gmail.com>
2024-10-08 21:33:16 -0600

    features redid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,27 +13,27 @@ const navigation = [
 
 const features = [
   {
-    name: 'Push to deploy',
+    name: '',
     description:
       'Morbi viverra dui mi arcu sed. Tellus semper adipiscing suspendisse semper morbi. Odio urna massa nunc massa.',
     icon: CloudArrowUpIcon,
   },
   {
-    name: 'SSL certificates',
+    name: '',
     description:
-      'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
+      '',
     icon: LockClosedIcon,
   },
   {
-    name: 'Simple queues',
+    name: '',
     description:
-      'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
+      '',
     icon: ArrowPathIcon,
   },
   {
-    name: 'Advanced security',
+    name: '',
     description:
-      'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
+      '',
     icon: FingerPrintIcon,
   },
 ]
